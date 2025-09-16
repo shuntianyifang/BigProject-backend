f36175179f9351910fe4217dcc9aa2e41dfb5c15
shuntianyifang <2016860013@qq.com>
2025-09-16 14:20:23 +0800

    test:关闭spring security方便测试

--- bigproject/src/main/java/org/bigseven/Main.java
+++ bigproject/src/main/java/org/bigseven/Main.java
@@ -3,6 +3,11 @@ package org.bigseven;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 @MapperScan("org.bigseven.mapper")
@@ -11,6 +16,15 @@ public class Main {
     public static void main(String[] args) {
 
         SpringApplication.run(Main.class, args);
+    }
+
+    /// 关闭spring security方便测试
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authz -> authz
+                .anyRequest().permitAll()
+        ).csrf(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 
 }
