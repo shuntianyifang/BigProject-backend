0831be8b0747fc8eef4a2895e16cabaa0d031853
v185v <67182630+v185v@users.noreply.github.com>
2025-09-16 08:20:17 +0800

    fix:添加了maven依赖，解决依赖不能正确解析 (#1)
    feat:添加了统一响应体和User实体类

---
+++ bigproject/src/main/java/org/bigseven/entity/User.java
@@ -0,0 +1,32 @@
+package org.bigseven.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * @TableName user
+ */
+@TableName(value ="user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+    private String realName;
+    private String phone;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+}
