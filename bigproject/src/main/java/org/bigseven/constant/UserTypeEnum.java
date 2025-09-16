de89cc1793281898be5abc70926811a2a78c00f9
v185v <67182630+v185v@users.noreply.github.com>
2025-09-16 22:16:54 +0800

    feat(User):增加用户类型枚举

--- bigproject/src/main/java/org/bigseven/constant/UserTypeEnum.java
+++ bigproject/src/main/java/org/bigseven/constant/UserTypeEnum.java
@@ -3,14 +3,14 @@ package org.bigseven.constant;
 import lombok.Getter;
 
 @Getter
-public enum RoleEnum {
+public enum UserTypeEnum {
     STUDENT("学生"),
     ADMIN("管理员"),
     SUPER_ADMIN("超级管理员");
 
     private final String displayName;
 
-    RoleEnum(String displayName) {
+    UserTypeEnum(String displayName) {
         this.displayName = displayName;
     }
 
