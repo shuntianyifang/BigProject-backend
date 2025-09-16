147cb967322fde0937d54dedfcc12c98928eb783
shuntianyifang <2016860013@qq.com>
2025-09-16 14:20:43 +0800

    test:关闭spring security方便测试

--- bigproject/src/main/java/org/bigseven/Main.java
+++ bigproject/src/main/java/org/bigseven/Main.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
