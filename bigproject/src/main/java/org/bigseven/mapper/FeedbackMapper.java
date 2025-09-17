9d46f1c3cc0772d039d892d069d03a20e092cde3
v185v <67182630+v185v@users.noreply.github.com>
2025-09-17 13:30:50 +0800

    refactor(feedback) (#12)
    * fix:添加了maven依赖，解决依赖不能正确解析
    feat:添加了统一响应体和User实体类
    
    * feat(User):完善User登录相关,完善项目结构
    
    * feat(User):完善User登录相关,完善项目结构
    
    * add API document
    
    * add API document
    
    * feat(User):增加用户类型枚举
    
    * feat(User):增加用户类型枚举
    
    * feat(User):增加用户类型枚举
    
    * 删除无用字段，规范register
    
    * 删除无用字段，规范register
    
    * feat(Post):完善基本学生发帖和管理员标记功能
    
    * refactor(feedback):将原来的Post实体类(学生问题帖子)改为Feedback实体类,将原来的管理员回复改为FeedbackReply实体类,新增学生评价实体类Rating
    
    * refactor(feedback):将原来的Post实体类(学生问题帖子)改为Feedback实体类,将原来的管理员回复改为FeedbackReply实体类,新增学生评价实体类Rating

---
+++ bigproject/src/main/java/org/bigseven/mapper/FeedbackMapper.java
@@ -0,0 +1,7 @@
+package org.bigseven.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.bigseven.entity.Feedback;
+
+public interface FeedbackMapper extends BaseMapper<Feedback> {
+}
