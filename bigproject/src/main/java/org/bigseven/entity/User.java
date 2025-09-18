0973a77d6ba8ea63818f05dee9b2af87d6a4a451
shuntianyifang <141918674+shuntianyifang@users.noreply.github.com>
2025-09-18 08:58:53 +0800

    fix:为用户类型枚举类增加了类型值 (#17)
    fix:为实体adminReply增加了时间相关的字段名
    fix:为实体user增加了userPhone和realName字段名

--- bigproject/src/main/java/org/bigseven/entity/User.java
+++ bigproject/src/main/java/org/bigseven/entity/User.java
@@ -30,12 +30,21 @@ public class User {
     private String password;
 
     private String email;
+
+    @TableField("user_phone")
+    @JsonProperty("user_phone")
+    private String userPhone;
+
     @TableField("user_type")
     @JsonProperty("user_type")
     private UserTypeEnum userType;
 
     // 直接用"nickname"，现代英语中"nickname"常以单独一个词使用
     private String nickname;
+
+    @TableField("real_name")
+    @JsonProperty("real_name")
+    private String realName;
 
     @TableField(value = "created_at", fill = FieldFill.INSERT)
     private LocalDateTime createdAt;
