dcb78c5be58c52875d3631b8f2f6a79fe51e6921
v185v <67182630+v185v@users.noreply.github.com>
2025-09-25 12:32:27 +0800

    feat(SuperAdmin):增加超级管理员相关操作(没写完) style(FeedbackServiceImpl):优化getAllFeedbacks方法

---
+++ bigproject/src/main/java/org/bigseven/dto/superadmin/SuperAdminMarkFeedbackResponse.java
@@ -0,0 +1,13 @@
+package org.bigseven.dto.superadmin;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SuperAdminMarkFeedbackResponse {
+}
