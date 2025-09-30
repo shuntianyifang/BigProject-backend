8ee17a965e3ccad63cc33fa57172709fc0fa120d
shuntianyifang <141918674+shuntianyifang@users.noreply.github.com>
2025-09-30 15:59:35 +0800

    chore:将processFeedback移动到FeedbackAdminController里 (#55)
    * chore:删除了部分无用的dto类
    chore:将markFeedback改为processFeedback
    
    * chore:将processFeedback移动到FeedbackAdminController里

--- bigproject/src/main/java/org/bigseven/controller/FeedbackAdminController.java
+++ bigproject/src/main/java/org/bigseven/controller/FeedbackAdminController.java
@@ -7,9 +7,12 @@ import org.bigseven.constant.FeedbackStatusEnum;
 import org.bigseven.constant.FeedbackTypeEnum;
 import org.bigseven.dto.feedback.GetAllFeedbackRequest;
 import org.bigseven.dto.feedback.GetAllFeedbackResponse;
+import org.bigseven.dto.feedback.PublishFeedbackRequest;
 import org.bigseven.result.AjaxResult;
+import org.bigseven.security.CustomUserDetails;
 import org.bigseven.service.FeedbackService;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -44,7 +47,23 @@ public class FeedbackAdminController {
     }
 
     /**
-     * 将反馈标记为垃圾信息
+     * 管理员处理反馈，标记反馈状态，可能做出回复
+     *
+     * @param request 包含标记反馈所需信息的请求对象，包括反馈ID和反馈状态
+     * @param userDetails 当前登录管理员的详细信息
+     * @return AjaxResult<Void> 操作结果，成功时返回空数据的成功响应
+     */
+    @PostMapping("/{id}/process")
+    public AjaxResult<Void> processFeedback(@Valid @RequestBody PublishFeedbackRequest request,
+                                            @AuthenticationPrincipal CustomUserDetails userDetails) {
+        // 从当前登录管理员中获取user_id，作为处理人ID
+        Integer acceptedByUserId = userDetails.getUserId();
+        feedbackService.processFeedback(request.getFeedbackId(), request.getFeedbackStatus());
+        return AjaxResult.success();
+    }
+
+    /**
+     * 将反馈标记为待确认垃圾信息
      * @param request 包含反馈ID和处理人员ID的请求对象
      * @return 包含处理结果的AjaxResult对象
      */
