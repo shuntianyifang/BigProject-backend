f165db914263aa9f1fb74409758f1f0bd4c0c47a
shuntianyifang <2016860013@qq.com>
2025-09-30 15:55:14 +0800

    chore:将processFeedback移动到FeedbackAdminController里

--- bigproject/src/main/java/org/bigseven/controller/FeedbackController.java
+++ bigproject/src/main/java/org/bigseven/controller/FeedbackController.java
@@ -3,14 +3,13 @@ package org.bigseven.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import jakarta.validation.Valid;
+import org.bigseven.dto.base.BaseListResponse;
 import org.bigseven.dto.feedback.GetAllFeedbackRequest;
 import org.bigseven.dto.feedback.GetAllFeedbackResponse;
-import org.bigseven.dto.base.BaseListResponse;
 import org.bigseven.dto.feedback.PublishFeedbackRequest;
 import org.bigseven.result.AjaxResult;
 import org.bigseven.security.CustomUserDetails;
 import org.bigseven.service.FeedbackService;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -80,20 +79,4 @@ public class FeedbackController {
         return AjaxResult.success(response);
     }
 
-    /**
-     * 管理员处理反馈，标记反馈状态，可能做出回复
-     *
-     * @param request 包含标记反馈所需信息的请求对象，包括反馈ID和反馈状态
-     * @param userDetails 当前登录管理员的详细信息
-     * @return AjaxResult<Void> 操作结果，成功时返回空数据的成功响应
-     */
-    @PostMapping("/{id}/process")
-    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
-    public AjaxResult<Void> processFeedback(@Valid @RequestBody PublishFeedbackRequest request,
-                                         @AuthenticationPrincipal CustomUserDetails userDetails) {
-        // 从当前登录管理员中获取user_id，作为处理人ID
-        Integer acceptedByUserId = userDetails.getUserId();
-        feedbackService.processFeedback(request.getFeedbackId(), request.getFeedbackStatus());
-        return AjaxResult.success();
-    }
 }
