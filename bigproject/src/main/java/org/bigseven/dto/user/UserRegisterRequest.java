c9cc4f4f19fef1b50a4f612fb0947ef83865c3c7
shuntianyifang <2016860013@qq.com>
2025-10-03 19:09:38 +0800

    fix:修复了注册时整不了昵称的问题 chore:删除了两个无用的类

--- bigproject/src/main/java/org/bigseven/dto/user/UserRegisterRequest.java
+++ bigproject/src/main/java/org/bigseven/dto/user/UserRegisterRequest.java
@@ -21,6 +21,10 @@ public class UserRegisterRequest {
     @Pattern(regexp = "^[a-zA-Z0-9_\\u4e00-\\u9fa5]{2,20}$", message = "用户名格式不正确")
     private String username;
 
+    @Size(min=1, max=20,message = "用户名长度必须在1-20之间")
+    @Pattern(regexp = "^[a-zA-Z0-9_\\u4e00-\\u9fa5]{2,20}$", message = "用户名格式不正确")
+    private String nickname;
+
     @NotBlank(message = "密码不能为空")
     private String password;
 
