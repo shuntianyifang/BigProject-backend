55d93544f5be2556c3603e3f167ae29bc79c1d6b
shuntianyifang <141918674+shuntianyifang@users.noreply.github.com>
2025-10-03 13:02:02 +0800

    refactor:通过新建几个工具类以去除feedback实现类中大量重复的代码 (#66)
    * chore:去除了部分未使用的import
    docs:规范了部分注释
    
    * feat:防护在注册时的XSS攻击
    
    * refactor:移除UserController中冗余的异常处理逻辑，简化代码
    
    * refactor:创建一个XssProtectionUtils以统一实现对前端发来数据的过滤和转义
    
    * refactor:更新PublishFeedbackRequest中的字段注解，重命名SecurityHeaderConfig包，优化UserServiceImpl中的异常处理
    
    * fix:修复了Jwt相关异常未能正确记录对应的异常种类
    
    * feat:根据feedback_id查询具体的反馈
    feat:新建UserConverterUtils以将User转化为UserSimpleVO
    fix:查询反馈列表时正确获取时间相关数据
    chore:向前端发送FeedbackStatusEnum的数据现在为整数型
    
    * refactor:优化了实现匿名功能的代码
    
    * refactor:去除了部分不必要的冗余
    
    * refactor:通过新建几个工具类以去除feedback实现类中大量重复的代码

---
+++ bigproject/src/main/java/org/bigseven/util/FeedbackImageUtils.java
@@ -0,0 +1,66 @@
+package org.bigseven.util;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.bigseven.entity.FeedbackImage;
+import org.bigseven.mapper.FeedbackImageMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 反馈图片工具类
+ *
+ * @author shuntianyifang
+ * &#064;date 2025/10/3
+ */
+@Component
+public class FeedbackImageUtils {
+
+    private final FeedbackImageMapper feedbackImageMapper;
+
+    @Value("${feedback.image-base-url:/api/file/images/}")
+    private String imageBaseUrl;
+
+    public FeedbackImageUtils(FeedbackImageMapper feedbackImageMapper) {
+        this.feedbackImageMapper = feedbackImageMapper;
+    }
+
+    /**
+     * 获取反馈图片URL列表
+     */
+    public List<String> getFeedbackImageUrls(Integer feedbackId) {
+        List<FeedbackImage> images = feedbackImageMapper.selectList(
+                new QueryWrapper<FeedbackImage>().eq("feedback_id", feedbackId)
+        );
+
+        return images.stream()
+                .map(FeedbackImage::getImageUrl)
+                .map(this::buildImageUrl)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * 构建完整的图片URL
+     */
+    public String buildImageUrl(String imagePath) {
+        return imageBaseUrl + imagePath;
+    }
+
+    /**
+     * 保存反馈图片
+     */
+    public void saveFeedbackImages(Integer feedbackId, List<String> imageUrls, int maxImages) {
+        if (imageUrls != null && !imageUrls.isEmpty()) {
+            for (int i = 0; i < Math.min(imageUrls.size(), maxImages); i++) {
+                FeedbackImage image = FeedbackImage.builder()
+                        .feedbackId(feedbackId)
+                        .imageUrl(imageUrls.get(i))
+                        .imageOrder(i)
+                        .build();
+                feedbackImageMapper.insert(image);
+            }
+        }
+    }
+}
