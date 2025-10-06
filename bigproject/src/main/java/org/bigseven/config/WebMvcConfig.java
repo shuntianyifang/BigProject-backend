899398a5b27d3c958110567742e0ea4033276032
v185v <67182630+v185v@users.noreply.github.com>
2025-10-07 03:29:18 +0800

    fix:尝试修复获取图片

--- bigproject/src/main/java/org/bigseven/config/WebMvcConfig.java
+++ bigproject/src/main/java/org/bigseven/config/WebMvcConfig.java
@@ -12,13 +12,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Value("${file.upload.path:uploads/}")
+    @Value("${file.upload.path:file/uploads/}")
     private String uploadPath;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 映射上传文件访问路径
-        registry.addResourceHandler("/uploads/**")
+        registry.addResourceHandler("/file/uploads/**")
                 .addResourceLocations("file:" + uploadPath);
     }
 }
