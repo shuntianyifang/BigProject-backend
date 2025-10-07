f5ab838659b88e63c1d08ded43bfe854d10eb679
shuntianyifang <2016860013@qq.com>
2025-10-07 11:34:45 +0800

    fix:试着修复图片

--- bigproject/src/main/java/org/bigseven/config/WebMvcConfig.java
+++ bigproject/src/main/java/org/bigseven/config/WebMvcConfig.java
@@ -13,12 +13,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Value("${file.upload-dir:uploads/}")
-    private String uploadPath;
+    private String uploadDir;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // 映射上传文件访问路径
+        // 添加静态资源映射，将 /uploads/** 映射到文件存储目录
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:" + uploadPath);
+                .addResourceLocations("file:" + uploadDir);
     }
 }
